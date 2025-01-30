292fccae0238a3e9fa8601c7e7d2c72bd28dfc2b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-30 22:06:26 +0600

    2nd trial also tle

--- C_Good_Array.py
+++ C_Good_Array.py
@@ -3,17 +3,13 @@ nums = list(map(int, input().split()))
 
 ans = []
 total = sum(nums)
-for i in range(len(nums)):
-    total -= nums[i]
-    for j in range(len(nums)):
-        if i==j: continue
-        
-        good_sum = total-nums[j]
-        if good_sum==nums[j]:
+for i in range(n):
+    for j in range(i+1, n):
+        good_sum = total-(nums[i]+nums[j])
+        if good_sum==nums[i]:
+            ans.append(j+1)
+        elif good_sum==nums[j]:
             ans.append(i+1)
-            break
-
-    total += nums[i]
 
 print(len(ans))
 for i in ans:
