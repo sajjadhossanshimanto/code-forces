89afaab06acd7ebe7aa6ed381b5245e8e9fb8f24
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-30 23:07:09 +0600

    ai solution

--- C_Good_Array.py
+++ C_Good_Array.py
@@ -39,3 +39,53 @@ print(len(ans))
 for i in ans:
     print(i, end=" ")
 print()
+#%% actual solution
+'''
+we have to sum all numbers then
+1. we must consider will there be any negative number
+    - if not then the sumation will always be greater and greater
+    - that means the sumation will be equal to the maximum number in the list 
+    - unless we are seleting that maximum_num for removal. 
+    - in that case the 2nd maxi will lead the sumation
+
+2. after removal of a number num[i]=sum of rest
+    - or we can say    num[i]+sum of rest => 2*num[i]
+
+
+'''
+# ai solution
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+total = sum(nums)  # Compute the sum of the entire array
+max1, max2 = 0, 0  # Track the largest and second largest elements
+max1_idx = -1      # Store the index of the largest element
+
+# Find the two largest elements in one pass (O(n))
+for i in range(n):
+    if nums[i] > max1:
+        max2 = max1
+        max1 = nums[i]
+        max1_idx = i
+    elif nums[i] > max2:
+        max2 = nums[i]
+
+ans = []
+
+# Check for "nice" indices in another pass (O(n))
+for i in range(n):
+    remaining_sum = total - nums[i]  # Sum after removing nums[i]
+
+    # If nums[i] is the largest element, check if remaining sum is twice max2
+    if nums[i] == max1:
+        if remaining_sum == 2 * max2:
+            ans.append(i + 1)  # Convert 0-based index to 1-based index
+    else:
+        if remaining_sum == 2 * max1:
+            ans.append(i + 1)
+
+# Output results
+print(len(ans))
+if ans:
+    print(*ans)
