d94e6fb546896db3faceb63e16d223918efc3f36
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-30 22:52:34 +0600

    all soltion

--- C_Good_Array.py
+++ C_Good_Array.py
@@ -1,3 +1,27 @@
+#%% sol 1
+n = int(input())
+nums = list(map(int, input().split()))
+
+ans = []
+total = sum(nums)
+for i in range(len(nums)):
+    total -= nums[i]
+    for j in range(len(nums)):
+        if i==j: continue
+        
+        good_sum = total-nums[j]
+        if good_sum==nums[j]:
+            ans.append(i+1)
+            break
+
+    total += nums[i]
+
+print(len(ans))
+for i in ans:
+    print(i, end=" ")
+print()
+
+#%% sol 2 different thinking
 n = int(input())
 nums = list(map(int, input().split()))
 
