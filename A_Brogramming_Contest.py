85f0774d2d9cf1f4b20d450358b85e2e14c7c13b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-17 17:07:20 +0600

    refined code

--- A_Brogramming_Contest.py
+++ A_Brogramming_Contest.py
@@ -1,3 +1,4 @@
+# basic implimentation
 for _ in range(int(input())):
     input()
     s = input()
@@ -15,5 +16,24 @@ for _ in range(int(input())):
             l-=1
         else:
             l-=1
+    
+    print(count)
+
+# refined code
+for _ in range(int(input())):
+    input()
+    s = input()
+    
+    count = 0
+    l = r =len(s)-1
+    while l>=0:
+        if s[l]=="1":
+            while s[l]!="0" and l>=0:
+                l-=1
+            count+=1
+            if s[r]=='0':
+                count+=1
+            r=l
+        l-=1
     
     print(count)
