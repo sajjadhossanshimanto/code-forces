969262762e0fab622a6194bc6a18d65af47f6afa
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-21 22:22:55 +0600

    function for repeated action

--- 1000/1. D_Three_Activities.py
+++ 1000/1. D_Three_Activities.py
@@ -1,5 +1,14 @@
 # https://codeforces.com/problemset/problem/1914/D
 
+def find_pos(list_var, i, mx_idx):
+
+    if list_var[i]>maxes[mx_idx][0][0]:
+        maxes[0][0] = (list_var[i], i)
+    elif list_var[i]>maxes[mx_idx][1][0]:
+        maxes[0][1] = (list_var[i], i)
+    elif list_var[i]>maxes[mx_idx][2][0]:
+        maxes[0][2] = (list_var[i], i)
+
 for _ in range(int(input())):
     n = int(input())
     sking = list(map(int, input().split()))
