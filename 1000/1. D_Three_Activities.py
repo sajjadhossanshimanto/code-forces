50aa17d95a606529975b15b18356d7bb661aaf0d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-21 23:53:15 +0600

    oveerthinking removed

--- 1000/1. D_Three_Activities.py
+++ 1000/1. D_Three_Activities.py
@@ -1,5 +1,4 @@
 # https://codeforces.com/problemset/problem/1914/D
-from itertools import combinations_with_replacement
 
 
 def find_pos(list_var, i, mx_idx):
@@ -8,16 +7,6 @@ def find_pos(list_var, i, mx_idx):
         if list_var[i]>maxes[mx_idx][j][0]:
             maxes[mx_idx][j] = (list_var[i], i)
             return
-
-def find_bst_2(i, j):
-    if i[1]==j[1]:
-        if i[0]>j[0]:
-            return True, False
-        return False, True
-    return True, True
-    
-def round_cycle(i):
-    return (i+1)%3
 
 for _ in range(int(input())):
     n = int(input())
@@ -34,37 +23,7 @@ for _ in range(int(input())):
         find_pos(movie, i, 1)
         find_pos(games, i, 2)
 
-    # ans = [0, 0, 0]
-    # ans[0], ans[1] = find_bst_2(sking, movie)
-    # find_bst_2(movie, games)
-
-    # if maxes[0][0][1]!=maxes[1][0][1]!=maxes[2][0][1]:
-    #     print()
-
-    # i, j, k = 0, 0, 0
-    # while ans[0]==ans[1] or ans[0]==ans[2] or ans[1]==ans[2]:
-    # while i==j or i==k or j==k:
-    #     if i==j:
-    #         if sking[i]>movie[j]:
-    #             j=round_cycle(j)
-    #         else:
-    #             i=round_cycle(i)
-    #     elif i==k:
-    #         if sking[i]>games[k]:
-    #             k=j=round_cycle(k)
-    #         else:
-    #             i=round_cycle(i)
-    #     elif j==k:
-    #         if movie[j]>games[k]:
-    #             k=j=round_cycle(k)
-    #         else:
-    #             j=round_cycle(j)
-
-    # for i, j, k in combinations_with_replacement(range(3), 3):
-    #     i, j, k = maxes[0][i], maxes[1][j], maxes[2][k]
-
-
-    # do not over think. try all the possible value and take the 
+    # do not over think. try all the possible value and take the. as there will be max 27 combinations
     ans = 0
     for i in maxes[0]:
         for j in maxes[1]:
