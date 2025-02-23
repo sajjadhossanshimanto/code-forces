680a3f76a75453be2aa0d9f380b0671f5c08e905
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-23 20:58:42 +0600

    with html input

--- url to image.py
+++ url to image.py
@@ -18,6 +18,38 @@ options = {
     # 'margin-bottom': '0.75in',
     # 'margin-left': '0.75in',
 }
-pdfkit.from_url(url, output_file, options=options)
+# pdfkit.from_url(url, output_file, options=options)
+
+#%% modified html with pdfkit
+import requests
+from bs4 import BeautifulSoup
+import pdfkit
+
+# Fetch the web page
+# url = 'https://example.com'
+response = requests.get(url)
+html_content = response.text
+
+
+# Use BeautifulSoup to parse and modify the HTML
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Example: Modify the title of the page
+# title_tag = soup.find(id="header")
+# if title_tag:
+#     title_tag.string = 'Modified Title'
+
+
+# Example: Add a new paragraph at the end
+new_paragraph = soup.new_tag('p')
+new_paragraph.string = 'This is a new paragraph added to the page.'
+soup.body.append(new_paragraph)
+
+# Convert the modified HTML to a PDF
+modified_html = str(soup)
+pdfkit.from_string(modified_html, 'output_pdfkit_from html.pdf')
+
+print("PDF generated with modified content!")
+
 
 # %%
