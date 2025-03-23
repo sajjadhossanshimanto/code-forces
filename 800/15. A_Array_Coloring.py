0ab267ceb28d2b2abc5179c845296d7cace23a07
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-23 22:50:56 +0600

    help note

--- 800/15. A_Array_Coloring.py
+++ 800/15. A_Array_Coloring.py
@@ -1,4 +1,11 @@
 # https://codeforces.com/problemset/problem/1857/A
+"""
+In this problem, parity refers to whether a number (or sum of numbers) is even or odd.
+
+- A number has even parity if it is divisible by 2 (e.g., 2, 4, 6, 8).
+- A number has odd parity if it is not divisible by 2 (e.g., 1, 3, 5, 7).
+"""
+
 
 for _ in range(int(input())):
     n = int(input())
