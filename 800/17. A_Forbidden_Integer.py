ab6853f669290f281f504019a0d0884bfbb74dd2
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-24 22:29:55 +0600

    print accordingly

--- 800/17. A_Forbidden_Integer.py
+++ 800/17. A_Forbidden_Integer.py
@@ -1,23 +1,29 @@
 # https://codeforces.com/problemset/problem/1845/A
 
-def target_sum(i, left_sum, path = []):
+def target_sum(i, left_sum, path):
     if left_sum==0: return True, path[:]
     if i==len(nums): return False, []
 
     # pick
     if nums[i]<=left_sum:
         path.append(nums[i])
-        r = target_sum(i, left_sum-nums[i])
+        r = target_sum(i, left_sum-nums[i], path)
         if r[0]:
             return r
         path.pop()
     
     # not-pick
-    return target_sum(i+1, left_sum)
+    return target_sum(i+1, left_sum, path)
 
 
 for _ in range(int(input())):
     target, k, x = map(int, input().split())
     nums = [i for i in range(k, 0, -1) if i!=x]
-    path = []
-    print(target_sum(0, target, path))
+    # path = []
+    r = target_sum(0, target, [])
+    if r[0]:
+        print("YES")
+        print(len(r[1]))
+        print(" ".join(map(str, r[1])))
+    else:
+        print("NO")
