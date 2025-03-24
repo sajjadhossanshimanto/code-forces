8c56c8726ad724324b7c9b1d8b30e3bd67f75fe4
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-24 23:43:57 +0600

    fixed printing and counting error

--- 800/17. A_Forbidden_Integer.py
+++ 800/17. A_Forbidden_Integer.py
@@ -1,38 +1,38 @@
-# https://codeforces.com/problemset/problem/1845/A
+# # https://codeforces.com/problemset/problem/1845/A
 
-def target_sum(i, left_sum, path):
-    if left_sum==0: return True, path[:]
-    if i==len(nums): return False, []
+# def target_sum(i, left_sum, path):
+#     if left_sum==0: return True, path[:]
+#     if i==len(nums): return False, []
 
-    # pick
-    if nums[i]<=left_sum:
-        path.append(nums[i])
-        r = target_sum(i, left_sum-nums[i], path)
-        if r[0]:
-            return r
-        path.pop()
+#     # pick
+#     if nums[i]<=left_sum:
+#         path.append(nums[i])
+#         r = target_sum(i, left_sum-nums[i], path)
+#         if r[0]:
+#             return r
+#         path.pop()
     
-    # not-pick
-    return target_sum(i+1, left_sum, path)
+#     # not-pick
+#     return target_sum(i+1, left_sum, path)
 
 
-for _ in range(int(input())):
-    target, k, x = map(int, input().split())
-    nums = [i for i in range(k, 0, -1) if i!=x]
+# for _ in range(int(input())):
+#     target, k, x = map(int, input().split())
+#     nums = [i for i in range(k, 0, -1) if i!=x]
     
-    dp = [[0]*(target+1) for _ in range(len(nums)+1)]
+#     dp = [[0]*(target+1) for _ in range(len(nums)+1)]
     
-    # base case
-    for i in range(len(nums)):
-        dp[i][0] = 1
+#     # base case
+#     for i in range(len(nums)):
+#         dp[i][0] = 1
     
-    # iterate
-    for i in range(len(nums)-1, -1, -1):
-        for left_sum in range(1, target+1):
-            dp[i][left_sum] = dp[i][left_sum-nums[i]] or dp[i+1][left_sum]
+#     # iterate
+#     for i in range(len(nums)-1, -1, -1):
+#         for left_sum in range(1, target+1):
+#             dp[i][left_sum] = dp[i][left_sum-nums[i]] or dp[i+1][left_sum]
     
-    print(dp[0][target], target_sum(0, target, []))
-    print(dp)
+#     print(dp[0][target], target_sum(0, target, []))
+#     print(dp)
 
     # if r[0]:
     #     print("YES")
@@ -40,6 +40,7 @@ for _ in range(int(input())):
     #     print(" ".join(map(str, r[1])))
     # else:
     #     print("NO")
+#%%
 
 for _ in range(int(input())):
     n, k, x = map(int, input().split())
@@ -64,10 +65,8 @@ for _ in range(int(input())):
             print("NO")
             continue
 
-        odd = n%2
-        if odd==1:
-            print("NO")
-        else:
-            print("YES")
-            print(n//2+1)
-            print(" ".join(["2"]*(n//2)), "3")
+        print("Yes")
+        print(int(n/2))
+        for i in range(n//2-1):
+            print("2", end=" ")
+        print("3")
