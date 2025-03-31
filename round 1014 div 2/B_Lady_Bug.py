df7c289728e8b8ec7ad1ec5500f18e4e47e6ae14
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-31 15:08:38 +0600

    ac code

--- round 1014 div 2/B_Lady_Bug.py
+++ round 1014 div 2/B_Lady_Bug.py
@@ -1,6 +1,6 @@
 # https://codeforces.com/contest/2092/problem/B
 
-
+#%% WA
 for _ in range(int(input())):
     n = int(input())
     a = input()
@@ -18,7 +18,7 @@ for _ in range(int(input())):
             a_even_1 += 1 if a[i]=='1' else 0
             b_even_0 += 1 if b[i]=='0' else 0
 
-    if (a_odd_1 and a_odd_1!=b_even_0) or (a_even_1 and a_even_1!=b_odd_0):
+    if (a_odd_1 and a_odd_1>b_even_0) or (a_even_1 and a_even_1>b_odd_0):
         print("NO")
     else:
         print("YES")
