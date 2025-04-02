be508da30b2a682c2323f1555bcf99186884cec7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-04-02 20:41:40 +0600

    better method

--- 900/29. A_Three_Indices.py
+++ 900/29. A_Three_Indices.py
@@ -22,3 +22,33 @@ for _ in range(int(input())):
 
     print("YES")
     print(i, j, j+1)
+
+#%%
+def rfind(arr, ele):
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i]==ele:
+            return i
+    
+    return -1
+
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    min_left = [nums[0]]
+    for i in range(1, n):
+        min_left.append(min(nums[i],  min_left[-1]))
+    # print(min_left)
+
+    min_right = [nums[-1]]
+    for i in range(n-2, -1, -1):
+        min_right.insert(0, min(nums[i],  min_right[0]))
+    # print(min_right)
+    
+    for i in range(1, n-1):
+        if  nums[i] > min_right[i+1] and min_left[i-1] < nums[i]:
+            print("YES")
+            print(nums.index(min_left[i]), i, rfind(nums, min_right[i]))
+            break
+    else:
+        print("NO")
