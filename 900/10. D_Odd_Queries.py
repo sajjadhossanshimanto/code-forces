86e22b071414c41d7b68db5f94e7c07e649e96e3
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-04-03 20:44:58 +0600

    ac

--- 900/10. D_Odd_Queries.py
+++ 900/10. D_Odd_Queries.py
@@ -5,13 +5,19 @@ for _ in range(int(input())):
     nums = list(map(int, input().split()))
 
     pre_sum = [nums[0]]
+    def range_sum(l, r):
+        if l==0:# the only edge case in prefix sum
+            return pre_sum[r]
+
+        return pre_sum[r]-pre_sum[l-1]
+    
     for i in range(1, n):
         pre_sum.append(pre_sum[-1]+nums[i])
     
     while q:
         l, r, k = map(int, input().split())
         l, r = l-1, r-1 # one indexed
-        if ((r-l+1)*k + (pre_sum[-1] - pre_sum[r]) + (pre_sum[l-1]))&1:
+        if ((r-l+1)*k + (pre_sum[-1] - range_sum(l, r)))&1:
             print("YES")
         else:
             print("NO")
