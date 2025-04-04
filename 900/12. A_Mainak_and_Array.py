97f171c683cf80a902e43a57b52977f421958758
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-04-04 19:37:41 +0600

    2nd implimentation

--- 900/12. A_Mainak_and_Array.py
+++ 900/12. A_Mainak_and_Array.py
@@ -8,3 +8,30 @@ for _ in range(int(input())):
     print(max(nums) - min(nums))
 
 #%%
+# TODO: issu with multiple occurance
+def arg_min_max(nums):
+    mx, mn = 0, 0
+    for i in range(len(nums)):
+        if nums[i] > nums[mx]:
+            mx = i
+        if nums[i] < nums[mn]:
+            mn = i
+    
+    return mn, mx
+
+for _ in range(int(input())):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    min_pos, max_pos = arg_min_max(nums)
+    if (max_pos==n-1 or min_pos==0) or (max_pos+1==min_pos):
+        # if max min already in 0 or n or 
+        # max is placed just in front of min
+        print(nums[max_pos] - nums[min_pos])
+    else:
+        # need to sacrifise one
+        if nums[0]-nums[min_pos] > nums[max_pos] - nums[n-1]:
+            print(nums[n-1] - nums[min_pos])
+        else:
+            print(nums[max_pos] - nums[0])
+    
