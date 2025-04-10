6f5f929ca5b3ef3f5be587a6f025e422882b944d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-04-10 22:36:40 +0600

    prime checker with 6k+-1

--- round 1016 div 3/C prime check.py
+++ round 1016 div 3/C prime check.py
@@ -2,12 +2,11 @@
 
 
 def is_prime(num):
-    if num==1: return False
-    if num<=3: return True
-    if num&1==0 or num%3==0: return False
+    if num==3 or num==2: return True
+    if num<=1 or num%2==0 or num%3==0: return False
 
     for i in range(5, int(pow(num, .5))+1, 6):
-        if num%i==0 or num%(i+2): 
+        if num%i==0 or num%(i+2)==0: 
             return False
     
     return True
