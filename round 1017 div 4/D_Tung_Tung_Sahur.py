1b041c37088427e2678c5e18ee43680eb52fb601
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-04-13 23:07:19 +0600

    wa at 2

---
+++ round 1017 div 4/D_Tung_Tung_Sahur.py
@@ -0,0 +1,37 @@
+# https://codeforces.com/contest/2094/problem/D
+
+def same_char(l, s):
+    r=l
+    while r<len(s) and s[r]==s[l]:
+        r+=1
+    
+    return l, r
+
+for _ in range(int(input())):
+    p = input()
+    s = input()
+    
+    l, r = 0, 0
+    l1, r1 = 0, 0
+    flag = True
+    while l<len(p):
+        l, r = same_char(l, p)
+        l1, r1 = same_char(l1, s)
+
+        if r1-l1>2*(r-l) or r1-l1<(r-l):
+            flag = False
+            break
+        
+        l = r
+        l1 = r1
+    
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+
+        
+
+        
+
+    
